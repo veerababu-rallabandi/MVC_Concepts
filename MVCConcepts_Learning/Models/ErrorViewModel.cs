6f915c0a846f218a1b63e174de7dd79857b46373
veerababu.rallabandi@gmail.com <veerababu.rallabandi@gmail.com>
2022-03-23 22:43:04 +0530

    all files

---
+++ MVCConcepts_Learning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCConcepts_Learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
