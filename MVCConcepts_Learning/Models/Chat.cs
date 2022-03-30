d6f851451274aef1e1a4dce39560431bcc846af9
veerababu.rallabandi@gmail.com <veerababu.rallabandi@gmail.com>
2022-03-31 01:08:41 +0530

    Add SignalR Logic

---
+++ MVCConcepts_Learning/Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCConcepts_Learning.Models
+{
+    public class Chat:Hub
+    {
+        public async Task sendMessage(string sender,string receiver,string msgTitle,string MessageBody)
+        {
+            await Clients.All.SendAsync("Receive message", sender, receiver, msgTitle, MessageBody);
+        }
+    }
+}
---
+++ MVCConcepts_Learning/Models/EmailDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCConcepts_Learning.Models
+{
+    public class EmailDetails
+    {
+        public string  EmailId { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
